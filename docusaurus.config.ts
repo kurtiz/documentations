654fbb7f840819622283ba0e738141a494369440
Aaron Will Djaba <papiliocurtis@gmail.com>
2025-02-04 17:20:19 +0000

    testing i18n

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,31 @@ const config: Config = {
     // may want to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'en',
-        locales: ['en', 'fr'],
+        locales: ['en', 'fa', 'fr'],
+        path: 'i18n',
+        localeConfigs: {
+            en: {
+                label: 'English',
+                direction: 'ltr',
+                htmlLang: 'en-US',
+                calendar: 'gregory',
+                path: 'en',
+            },
+            fa: {
+                label: 'فارسی',
+                direction: 'rtl',
+                htmlLang: 'fa-IR',
+                calendar: 'persian',
+                path: 'fa',
+            },
+            fr: {
+                label: 'Français',
+                direction: 'ltr',
+                htmlLang: 'fr-FR',
+                calendar: 'gregory',
+                path: 'fr',
+            },
+        },
     },
 
     presets: [
@@ -61,11 +85,11 @@ const config: Config = {
             } satisfies Preset.Options,
         ],
     ],
-
     themeConfig: {
         // Replace with your project's social card
         image: 'img/docusaurus-social-card.jpg',
         navbar: {
+            hideOnScroll: true,
             title: 'Documentations | by kurtiz',
             logo: {
                 alt: 'My Site Logo',
@@ -76,9 +100,24 @@ const config: Config = {
                     type: 'docSidebar',
                     sidebarId: 'tutorialSidebar',
                     position: 'left',
-                    label: 'Docs',
+                    label: 'Documentations',
                 },
                 // {to: '/blog', label: 'Blog', position: 'left'},
+
+                {
+                    type: 'localeDropdown',
+                    position: 'right',
+                    dropdownItemsAfter: [
+                        {
+                            type: 'html',
+                            value: '<hr style="margin: 0.3rem 0;">',
+                        },
+                        {
+                            href: 'https://github.com/kurtiz/documentations/issues/new?template=Blank+issue',
+                            label: 'Help Us Translate',
+                        },
+                    ],
+                },
                 {
                     href: 'https://github.com/kurtiz/documentations',
                     label: 'GitHub',
@@ -93,7 +132,7 @@ const config: Config = {
                     title: 'Docs',
                     items: [
                         {
-                            label: 'Tutorial',
+                            label: 'Documentations',
                             to: '/docs/intro',
                         },
                     ],
@@ -116,16 +155,62 @@ const config: Config = {
                     ],
                 },
                 {
-                    title: 'More',
+                    title: 'Socials',
                     items: [
                         // {
                         //   label: 'Blog',
                         //   to: '/blog',
                         // },
                         {
-                            label: 'GitHub',
-                            href: 'https://github.com/kurtiz/documentations',
+                           html: `<div style="display: grid; grid-template-columns: 0.5fr 0.5fr; gap: 10px: width: 100px;">
+                                        <div style="display: flex; flex-wrap:wrap; align-items: center;">
+                                            <a href="https://github.com/kurtiz" target="_blank" rel="noopener noreferrer" style="text-decoration:none;border:0;width:25px;height:25px;padding:2px;margin:5px;color:#303846;border-radius:50%;background-color:#ffffff;">
+                                                <svg class="niftybutton-github" style="display:block;fill:currentColor" data-donate="true" data-tag="git" data-name="Github" viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><title>Github social icon</title>
+                                                    <path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"></path>
+                                                </svg>
+                                                
+                                            </a>
+                                            <a href="https://github.com/kurtiz" target="_blank" rel="noopener noreferrer" style="color: white">Github</a>
+                                        </div>
+                                        
+                                        <div style="display: flex; flex-wrap:wrap; align-items: center;">
+                                        <a href="https://x.com/brakhobbykurtiz" target="_blank" rel="noopener noreferrer" style="text-decoration:none;border:0;width:25px;height:25px;padding:2px;margin:5px;color:#303846;border-radius:50%;background-color:#ffffff;">
+                                            <svg class="niftybutton-twitterx" style="display:block;fill:currentColor" data-donate="true" data-tag="twix" data-name="TwitterX" viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><title>Twitter X social icon</title>
+                                                <path d="M 304.757 216.824 L 495.394 0 L 450.238 0 L 284.636 188.227 L 152.475 0 L 0 0 L 199.902 284.656 L 0 512 L 45.16 512 L 219.923 313.186 L 359.525 512 L 512 512 M 61.456 33.322 L 130.835 33.322 L 450.203 480.317 L 380.811 480.317 ">
+                                                </path>
+                                            </svg>              
+                                        </a>
+                                        <a href="https://github.com/kurtiz" target="_blank" rel="noopener noreferrer" style="color: white">x.com</a>
+                                    </div>
+                                    
+                                    <div style="display: flex; flex-wrap:wrap; align-items: center;">
+                                        <a href="https://instagram.com/brakhobbykurtiz" target="_blank" rel="noopener noreferrer" style="text-decoration:none;border:0;width:25px;height:25px;padding:2px;margin:5px;color:#303846;border-radius:50%;background-color:#ffffff;">
+                                            <svg class="niftybutton-instagram" style="display:block;fill:currentColor" data-donate="true" data-tag="ins" data-name="Instagram" viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><title>Instagram social icon</title>
+                                                <path d="M256 109.3c47.8 0 53.4 0.2 72.3 1 17.4 0.8 26.9 3.7 33.2 6.2 8.4 3.2 14.3 7.1 20.6 13.4 6.3 6.3 10.1 12.2 13.4 20.6 2.5 6.3 5.4 15.8 6.2 33.2 0.9 18.9 1 24.5 1 72.3s-0.2 53.4-1 72.3c-0.8 17.4-3.7 26.9-6.2 33.2 -3.2 8.4-7.1 14.3-13.4 20.6 -6.3 6.3-12.2 10.1-20.6 13.4 -6.3 2.5-15.8 5.4-33.2 6.2 -18.9 0.9-24.5 1-72.3 1s-53.4-0.2-72.3-1c-17.4-0.8-26.9-3.7-33.2-6.2 -8.4-3.2-14.3-7.1-20.6-13.4 -6.3-6.3-10.1-12.2-13.4-20.6 -2.5-6.3-5.4-15.8-6.2-33.2 -0.9-18.9-1-24.5-1-72.3s0.2-53.4 1-72.3c0.8-17.4 3.7-26.9 6.2-33.2 3.2-8.4 7.1-14.3 13.4-20.6 6.3-6.3 12.2-10.1 20.6-13.4 6.3-2.5 15.8-5.4 33.2-6.2C202.6 109.5 208.2 109.3 256 109.3M256 77.1c-48.6 0-54.7 0.2-73.8 1.1 -19 0.9-32.1 3.9-43.4 8.3 -11.8 4.6-21.7 10.7-31.7 20.6 -9.9 9.9-16.1 19.9-20.6 31.7 -4.4 11.4-7.4 24.4-8.3 43.4 -0.9 19.1-1.1 25.2-1.1 73.8 0 48.6 0.2 54.7 1.1 73.8 0.9 19 3.9 32.1 8.3 43.4 4.6 11.8 10.7 21.7 20.6 31.7 9.9 9.9 19.9 16.1 31.7 20.6 11.4 4.4 24.4 7.4 43.4 8.3 19.1 0.9 25.2 1.1 73.8 1.1s54.7-0.2 73.8-1.1c19-0.9 32.1-3.9 43.4-8.3 11.8-4.6 21.7-10.7 31.7-20.6 9.9-9.9 16.1-19.9 20.6-31.7 4.4-11.4 7.4-24.4 8.3-43.4 0.9-19.1 1.1-25.2 1.1-73.8s-0.2-54.7-1.1-73.8c-0.9-19-3.9-32.1-8.3-43.4 -4.6-11.8-10.7-21.7-20.6-31.7 -9.9-9.9-19.9-16.1-31.7-20.6 -11.4-4.4-24.4-7.4-43.4-8.3C310.7 77.3 304.6 77.1 256 77.1L256 77.1z"></path>
+                                                <path d="M256 164.1c-50.7 0-91.9 41.1-91.9 91.9s41.1 91.9 91.9 91.9 91.9-41.1 91.9-91.9S306.7 164.1 256 164.1zM256 315.6c-32.9 0-59.6-26.7-59.6-59.6s26.7-59.6 59.6-59.6 59.6 26.7 59.6 59.6S288.9 315.6 256 315.6z"></path>
+                                                <circle cx="351.5" cy="160.5" r="21.5"></circle>
+                                            </svg>
+                                        </a>
+                                        <a href="https://github.com/kurtiz" target="_blank" rel="noopener noreferrer" style="color: white">Instagram</a>
+                                    </div>
+                                        
+                                    <div style="display: flex; flex-wrap:wrap; align-items: center;">
+                                        <a href="https://www.linkedin.com/in/aaron-will-djaba-424b7a184/" target="_blank" rel="noopener noreferrer" style="text-decoration:none;border:0;width:24px;height:24px;padding:2px;margin:5px;color:#303846;border-radius:50%;background-color:#ffffff;">
+                                            <svg class="niftybutton-linkedin" style="display:block;fill:currentColor" data-donate="true" data-tag="lin" data-name="LinkedIn" viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><title>LinkedIn social icon</title>
+                                                <path d="M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z"></path>
+                                            </svg>
+                                        </a>
+                                        <a href="https://github.com/kurtiz" target="_blank" rel="noopener noreferrer" style="color: white">LinkedIn</a>
+                                    </div>
+                                      
+                                </div>`
                         },
+
+                        {
+                            label: 'Quora',
+                            href: 'https://www.quora.com/profile/Aaron-Will-Djaba',
+                        },
+
                     ],
                 },
             ],
